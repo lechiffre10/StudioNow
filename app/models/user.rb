ca7dafbbc3a688eab82cfe1aa5449dfa18c400fa
ray-curran <rpcurr@gmail.com>
2016-03-10 16:10:58 -0600

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  has_many :bookings
+  has_many :studios, foreign_key: :owner_id
+  has_many :submitted_ratings, foreign_key: :rater_id
+  has_many :ratings, as: :ratable
+  has_many :written_reviews, foreign_key: :reviewer_id
+  has_many :reviews, as: :reviewable
+
+  validates :username, presence: true, uniqueness: true
+  validates :first_name, :last_name, :email, presence: true
+  has_attached_file :image, :styles => {:standard => "150x150"}
+  validates_attachment :image, :content_type => {:content_type => /^image\/jpeg|png|gif|tiff)$/}
+  validates :password, :length => { :within => 6..20 }
+
 end
