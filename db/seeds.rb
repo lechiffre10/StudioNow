342d70640edd520d89203abb7aeaf1355a6c2c43
cstallings1 <catiestallings1@gmail.com>
2016-03-10 16:31:55 -0600

    Add user and studio seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Studio.destroy_all
+
+
+users = 15.times.map do
+  User.create!(username: Faker::Internet.user_name, password_digest: "password", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::Hipster.sentence(rand(3..20)), email: Faker::Internet.email, genres: Faker::Book.genre)
+end
+
+studio_owners = 15.times.map do
+  User.create!(username: Faker::Internet.user_name, password_digest: "password", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, description: Faker::StarWars.quote, email: Faker::Internet.email, genres: Faker::Book.genre)
+end
+
+nums = [40.00, 35.50, 25.25, 88.50, 90.00, 33.40, 50.00, 55.00]
+
+studios = 15.times.map do
+  Studio.create!(name: Faker::Team.name, user: studio_owners.rotate![0], address: Faker::Address.street_address, lat: Faker::Address.latitude, lng: Faker::Address.longitude, city: Faker::Address.city, state: Faker::Address.state, description: Faker::Hipster.sentence(rand(3..20)), price: nums.rotate![0], website: Faker::Internet.url)
+end
+
+15.times.map do
+  users.sample.rating.create!(value: rand(1..5), studios.sample)
+end
+
