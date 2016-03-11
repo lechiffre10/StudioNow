c7db897cef47c88ccbf576004993603efac60744
Tiffany Larson <tslarson@gmail.com>
2016-03-11 14:51:15 -0600

    Add shoulda matchers tests for user, studio, review, rating, image

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,8 +16,6 @@ describe User do
   it { is_expected.to validate_presence_of(:email) }		
   it { is_expected.to validate_length_of(:password).is_at_least(6) }		
   it { is_expected.to validate_length_of(:password).is_at_most(20) }
-
-
 	end
 end
 
