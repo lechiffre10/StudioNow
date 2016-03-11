76347a9add9776c0992f170e9cfb42a3eac22ba7
Tiffany Larson <tslarson@gmail.com>
2016-03-11 15:33:50 -0600

    add login page and set session

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,13 @@ class UsersController < ApplicationController
   end
 
   def create
-
+    user = User.create(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to "/users/#{user.id}"
+    else
+      redirect_to :back
+    end
   end
 
   def edit
@@ -21,6 +27,11 @@ class UsersController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :username, :password, :description, :email, :genres)
   end
 
 
