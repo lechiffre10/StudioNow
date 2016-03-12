78bb70778d482d5aeb25d6a83e25e3ffee8a44c8
Catie Stallings <email": null>
2016-03-12 11:16:18 -0600

    Fix bugs in Google map on index page

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -1,15 +1,28 @@
 class StudiosController < ApplicationController
   def index
-    if params[:search]
+    if params[:search] && params[:search] != ''
+
       @studios = Studio.search(params[:search])
+
       @hash = Gmaps4rails.build_markers(@studios) do |studio, marker|
         marker.lat studio.latitude
         marker.lng studio.longitude
       end
-      puts "***************"
-      puts @hash
+
+      matchers = Geocoder.search(params[:search])
+
+      if !matchers.empty?
+        @searched_coords = matchers[0].coordinates
+      else
+        @hash = []
+        @searched_coords = ['soooooooooOOOOOooooooOOOOOOOoooooooooOOOOOOOOOOOo', 'mOOse']
+        @studios = nil
+      end
+
     else
       @studios = nil
+      @hash = []
+      @searched_coords = ['catie', 'stinks']
     end
   end
 
