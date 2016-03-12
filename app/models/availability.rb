a0f980635bb401251293adba24adabfbe809815f
Tiffany Larson <tslarson@gmail.com>
2016-03-12 13:51:47 -0600

    Add availability start and end time to studio page

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -4,4 +4,12 @@ class Availability < ActiveRecord::Base
 
   validates :start_time, :end_time, presence: true
 
+
+  def pretty_print_start_time 
+  	self.start_time.strftime('%a, %d %b %Y %H:%M:%S')
+  end
+
+ 	def pretty_print_end_time 
+  	self.end_time.strftime('%a, %d %b %Y %H:%M:%S')
+  end
 end
