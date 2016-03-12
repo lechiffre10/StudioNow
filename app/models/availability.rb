23b4a13cb4d4e745391f94ea69ec6744b8dac034
Tiffany Larson <tslarson@gmail.com>
2016-03-12 14:43:24 -0600

    still working on sorting date times

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -4,12 +4,31 @@ class Availability < ActiveRecord::Base
 
   validates :start_time, :end_time, presence: true
 
-
-  def pretty_print_start_time 
-  	self.start_time.strftime('%a, %d %b %Y %H:%M:%S')
+  def unbooked_times
+    all_times = [self.start_time]
+    all_bookings = self.bookings.sort_by {|booking| booking.start_time }
+    until all_bookings.length == 0 do 
+    	all_times << all_bookings.first.start_time
+    	all_times << all_bookings.first.end_time
+    	all_bookings.shift
+    end
+    all_times << self.end_time
+    create_multi_level_array(all_times)
   end
 
- 	def pretty_print_end_time 
-  	self.end_time.strftime('%a, %d %b %Y %H:%M:%S')
+  def create_multi_level_array(array)
+  	final_array = []
+  	i = 0
+  	until i == (array.length - 1) do 
+  		final_array << [beautify(array[i]), beautify(array[i+1])]
+  		i += 1
+  	end
+  	final_array
   end
+
+
+  def beautify(time) 
+  	time.strftime('%a, %d %b %Y %H:%M:%S')
+  end
+
 end
