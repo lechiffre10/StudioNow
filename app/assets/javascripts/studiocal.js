451810fe7e47db097e6a88dd372e23eaa615491a
Sabri Helal & Ray Curran <pair+lechiffre10.ray-curran@devbootcamp.com>
2016-03-13 17:44:24 -0500

    Add calendar in studio show page

---
+++ app/assets/javascripts/studiocal.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+  $('#studiocal').fullCalendar({
+    header: {
+      left: '',
+      center: 'title',
+      right: ''
+    },
+    editable: false,
+    allDaySlot: false,
+    defaultView: 'month',
+    height: 300,
+    aspectRatio: 2,
+    slotMinutes: 60,
+    loading: function(bool){
+      if (bool)
+        $('#loading').show();
+      else
+        $('#loading').hide();
+    },
+    events: "/availabilities/get_slots",
+    timeFormat: 'hh:mm t{ - hh:mm t} '
+  });
+});
+
+
+
