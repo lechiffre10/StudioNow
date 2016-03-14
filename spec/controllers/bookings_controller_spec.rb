319cf67c79a21870c9ab15b056585f1a11793615
Sabri Helal & Ray Curran <pair+lechiffre10.ray-curran@devbootcamp.com>
2016-03-14 11:03:41 -0500

    Add tests for booking controller, still not sure how to test the booking_params, so we are only at 95% on this one :(

--- spec/controllers/bookings_controller_spec.rb
+++ spec/controllers/bookings_controller_spec.rb
@@ -9,14 +9,36 @@ RSpec.describe BookingsController, type: :controller do
   end
 
   def create_booking
-    post :create, start_date: '4/26/2016', end_date: '4/26/2016', s_time: '1:00am', e_time: '2:00am', studio_id: @studio1.id
+    post :create, start_date: '4/26/2016', end_date: '4/26/2016', s_time: '1:00am', e_time: '2:00am', studio_id: @studio1.id, availability_id: @availability1.id, user_id: @user1.id
   end
 
   describe '#create' do
+    before { allow(controller).to receive(:current_user) {@user1}}
+
     it 'creates a booking when it gets the right data and there is an available time' do
-      before { allow(controller).to receive(:current_user) {@user1}}
       create_booking
-      expect(Booking.last.start_time).to eq('something')
+      expect(Booking.last.start_time).to eq('2016-04-26 01:00:00.000000000 +0000')
+      expect(Booking.last.end_time).to eq('2016-04-26 02:00:00.000000000 +0000')
+    end
+
+    it 'gives the right flash notice if submitted' do
+      create_booking
+      expect(flash[:notice]).to eq('Your booking has been submitted.')
+    end
+
+    it 'redirects to the studio show page on submission' do
+      create_booking
+      expect(response).to redirect_to("/studios/#{@studio1.id}")
+    end
+
+    it 'returns flash errors if not given the right parameters' do
+      post :create, start_date: '4/26/2016', end_date: '4/26/2016', s_time: '3:00am', e_time: '2:00am', studio_id: @studio1.id, availability_id: @availability1.id, user_id: @user1.id
+      expect(flash[:errors]).to eq ["Your booking start date cannot be after the end date"]
+    end
+
+    it 'return correct flash errors if no suitable availability is found' do
+      post :create, start_date: '4/26/2016', end_date: '4/26/2016', s_time: '12:00am', e_time: '1:00am', studio_id: @studio1.id, availability_id: @availability1.id, user_id: @user1.id
+      expect(flash[:errors]).to eq ["There are no availabilities that match those requested times."]
     end
   end
 
