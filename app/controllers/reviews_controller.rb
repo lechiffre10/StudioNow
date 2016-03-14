4216b72df5fd260b0e144d4302a3c705a73ef602
James O'Neal & TSLarson <pair+jamesponeal.tslarson@devbootcamp.com>
2016-03-14 11:05:45 -0500

    Add review with ajax to user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,22 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @user = User.find(params[:user_id])
+    @review = Review.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @review = @user.reviews.new(review_params)
+    if session[:user_id].nil?
+      flash[:notice] = "You cannot make a review without being logged in bro!"
+    else
+      @review[:reviewer_id] = session[:user_id]
+      @review.save
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
+  end
 end
