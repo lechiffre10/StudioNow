4dec3b928e3e70f2a067bdae51fbb55e9416fa40
James O'Neal & TSLarson <pair+jamesponeal.tslarson@devbootcamp.com>
2016-03-14 11:28:55 -0500

    add review form to studio

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,13 @@
 class ReviewsController < ApplicationController
 
   def new
-    @user = User.find(params[:user_id])
     @review = Review.new
+    params[:user_id].nil? ? @studio = Studio.find(params[:studio_id]) : @user = User.find(params[:user_id])
   end
 
   def create
-    @user = User.find(params[:user_id])
-    @review = @user.reviews.new(review_params)
+    params[:user_id].nil? ? this = Studio.find(params[:studio_id]) : this = User.find(params[:user_id])
+    @review = this.reviews.new(review_params)
     if session[:user_id].nil?
       flash[:notice] = "You cannot make a review without being logged in bro!"
     else
