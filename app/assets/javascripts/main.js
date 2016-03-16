f3e3acd0a7ec050b1fe0cef22dd86a93bece0309
Catie Stallings <email": null>
2016-03-15 21:10:18 -0500

    Make style changes to register, new studio pages

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,7 +6,7 @@
 
 function main() {
 
-(function () {
+  (function () {
    'use strict';
 
    /* ==============================================
@@ -14,119 +14,127 @@ function main() {
   	=============================================== */
 
   	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top - 40
+          }, 900);
+          return false;
         }
-      });
+      }
+    });
 
     /*====================================
     Show Menu on Book
     ======================================*/
     $(window).bind('scroll', function() {
-        var navHeight = $(window).height() - 100;
-        if ($(window).scrollTop() > navHeight) {
-            $('.navbar-default').addClass('on');
-        } else {
-            $('.navbar-default').removeClass('on');
-        }
+      var navHeight = $(window).height() - 100;
+      if ($(window).scrollTop() > navHeight) {
+        $('.navbar-default').addClass('on');
+      } else {
+        $('.navbar-default').removeClass('on');
+      }
     });
 
     $('body').scrollspy({
-        target: '.navbar-default',
-        offset: 80
+      target: '.navbar-default',
+      offset: 80
     })
 
-  	$(document).ready(function() {
-  	  $("#listings").owlCarousel({
+    $(document).ready(function() {
+     $("#listings").owlCarousel({
 
+  	      navigation : true, // Show next and prev buttons
+          navigationText: [
+          "<i class='icon-chevron-left icon-white'><</i>",
+          "<i class='icon-chevron-right icon-white'>></i>"
+          ],
+          slideSpeed : 300,
+          paginationSpeed : 400,
+          autoHeight : true,
+          itemsCustom : [
+          [0, 1],
+          [450, 2],
+          [600, 2],
+          [700, 2],
+          [1000, 4],
+          [1200, 4],
+          [1400, 4],
+          [1600, 4]
+          ],
+        });
+
+     $("#test").owlCarousel({
   	      navigation : false, // Show next and prev buttons
   	      slideSpeed : 300,
   	      paginationSpeed : 400,
   	      autoHeight : true,
   	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 4],
-				        [1400, 4],
-				        [1600, 4]
-				      ],
-  	  });
+          [0, 1],
+          [450, 2],
+          [600, 2],
+          [700, 2],
+          [1000, 4],
+          [1200, 5],
+          [1400, 5],
+          [1600, 5]
+          ],
+        });
 
-  	  $("#test").owlCarousel({
-  	      navigation : false, // Show next and prev buttons
-  	      slideSpeed : 300,
-  	      paginationSpeed : 400,
-  	      autoHeight : true,
-  	      itemsCustom : [
-				        [0, 1],
-				        [450, 2],
-				        [600, 2],
-				        [700, 2],
-				        [1000, 4],
-				        [1200, 5],
-				        [1400, 5],
-				        [1600, 5]
-				      ],
-  	  });
+     $("#detail-photos").owlCarousel({
+        navigation : true, // Show next and prev buttons
+        navigationText: [
+        "<i class='icon-chevron-left icon-white'><</i>",
+        "<i class='icon-chevron-right icon-white'>></i>"
+        ],
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        singleItem:true,
+      });
 
-      $("#detail-photos").owlCarousel({
+     $("#profile-studio-photos").owlCarousel({
         navigation : false, // Show next and prev buttons
         slideSpeed : 300,
         paginationSpeed : 400,
         singleItem:true
-        });
-
-      $("#profile-studio-photos").owlCarousel({
-        navigation : false, // Show next and prev buttons
-        slideSpeed : 300,
-        paginationSpeed : 400,
-        singleItem:true
-        });
-  	});
+      });
+   });
 
   	/*====================================
     Portfolio Isotope Filter
     ======================================*/
     $(window).load(function() {
-        var $container = $('#lightbox');
+      var $container = $('#lightbox');
+      $container.isotope({
+        filter: '*',
+        animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
+        }
+      });
+      $('.cat a').click(function() {
+        $('.cat .active').removeClass('active');
+        $(this).addClass('active');
+        var selector = $(this).attr('data-filter');
         $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
+          filter: selector,
+          animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+          }
         });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
+        return false;
+      });
 
     });
 
 
 
-}());
+  }());
 
 
 }
