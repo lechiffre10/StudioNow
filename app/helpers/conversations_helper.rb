80afad1ece63e7125a7c543c7a09bf98a608114e
Catie Stallings <email": null>
2016-03-17 10:40:09 -0500

    Style messages

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -8,6 +8,14 @@ module ConversationsHelper
     User.find_by(id: conversation.recipient_id).username
   end
 
+  def find_originator(conversation)
+    User.find_by(id: conversation.originator_id)
+  end
+
+  def find_recipient(conversation)
+    User.find_by(id: conversation.recipient_id)
+  end
+
   def find_studio_id(conversation)
     Studio.find_by(owner_id: conversation.recipient_id)
   end
